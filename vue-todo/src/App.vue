b0e5f4eefe2af95454dd2aac9be14a7d6d2e08b3
choisoobong <devcsb119@gmail.com>
2022-06-28 17:13:44 +0900

    프로젝트 구현 - 컴포넌트 생성 및 등록하기

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader> <!--PascalCase vs kebab-case 선택-->
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+<todo
+  </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import TodoFooter from "./components/TodoFooter";
+
+// // es5의 방식
+// var my_cmp = {
+//   template: '<div>my component</div>'
+// };
+//
+// new Vue({
+//   el: '',
+//   components: {
+//     // '컴포넌트 이름': '컴포넌트 내용'
+//     'my-cmp': my_cmp
+//   }
+// })
+export default {
+  components: {
+    // 컴포넌트 태그 명 : 컴포넌트 내용
+    'TodoHeader': TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter,
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div><!-- 컴포넌트의 root 템플릿은 1개여야만 한다-->
+    header
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "TodoHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
